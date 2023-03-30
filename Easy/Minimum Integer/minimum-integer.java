d720feb223084046f4d24291f234038d436ba966
Divyansh Agarwal <90123262+divyanshagarwall@users.noreply.github.com>
2023-03-30 10:16:27 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Integer/minimum-integer.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.minimumInteger(N, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int minimumInteger(int N, int[] A) {
+        // code here
+        int ans = Integer.MAX_VALUE;
+        long sum  = 0;
+        for (Integer itr:A) {
+            sum+=itr;
+        }
+        long div =  sum/N;
+        long temp  = 0;
+        if(sum == div *  N)
+        {
+            temp  = div;
+        }
+        else{
+            temp =  div+1;
+        }
+        for(int i = 0; i<A.length; i++)
+        {
+            if(A[i]>=temp)
+            {
+                ans = Math.min(A[i], ans);
+            }
+        }
+        return ans;
+
+
+    }
+}
+        
