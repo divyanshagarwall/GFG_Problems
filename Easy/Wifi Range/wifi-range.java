62b38417b1006abacfe6a7f2f2124fa9b975fc2d
Divyansh Agarwal <90123262+divyanshagarwall@users.noreply.github.com>
2023-04-19 12:52:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Wifi Range/wifi-range.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(input_line[0]);
+            int X = Integer.parseInt(input_line[1]);
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            boolean ans = ob.wifiRange(N, S, X); 
+            if(ans)
+            System.out.println(1);
+            else
+            System.out.println(0);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    boolean wifiRange(int N, String S, int X) 
+    { 
+        // code here
+        int curr = 0;
+        for(int i=0;i<S.length();i++) {
+            if(S.charAt(i)=='0') {
+                curr++;
+            } else {
+                if(curr > X) {
+                    return false;
+                }
+                curr =-X;
+            }
+        }
+        if(curr > 0) {
+            return false;
+        }
+        return true;
+
+    }
+} 
