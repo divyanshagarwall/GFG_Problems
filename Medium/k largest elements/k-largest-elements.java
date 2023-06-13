2f63481101ae6fe70c4cfd126e2861558085c0c0
Divyansh Agarwal <90123262+divyanshagarwall@users.noreply.github.com>
2023-06-13 17:58:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/k largest elements/k-largest-elements.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        // code here
+        int a[]=new int[k];
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<k;i++)
+        {
+          pq.add(arr[i]);  
+        }
+        for(int i=k;i<n;i++)
+        {
+            if(pq.peek()<arr[i])
+            {
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        while(!pq.isEmpty())
+        {
+            a[k-1]=pq.poll();
+            k-=1;
+        }
+        return a; 
+    }
+}
