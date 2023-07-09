b825d35d71f7b07fe2c3735e22eeac02aae6d4a1
Divyansh Agarwal <90123262+divyanshagarwall@users.noreply.github.com>
2023-07-09 11:35:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smallest Positive missing number/smallest-positive-missing-number.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],0);
+        }
+        for(int i=1;i<arr.length+1;i++){
+            if(!map.containsKey(i)){
+                return i;
+            }
+        }
+       return arr.length+1;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main
+{   
+    public static void main (String[] args) 
+    {
+
+		Scanner sc=new Scanner(System.in);
+		
+        //taking testcases
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    //input number n
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=sc.nextInt();
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			System.out.println(missing);
+		}
+    }
+}
+
+
+// } Driver Code Ends
